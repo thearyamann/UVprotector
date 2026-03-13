4c5d0e906de43ffe260da4f4cb48137e5d972605
Aryamann Chaudhary <thearyamann15151@gmail.com>
2026-03-14 00:05:43 +0530

    some more updates

--- lib/services/uv_controller.dart
+++ lib/services/uv_controller.dart
@@ -2,7 +2,7 @@ import 'package:uv_index_app/services/location_service.dart';
 import 'package:uv_index_app/services/uv_api_service.dart';
 import 'package:uv_index_app/engines/uv_risk_engine.dart';
 import 'package:uv_index_app/models/uv_data.dart';
-
+import 'package:uv_index_app/engines/sun_exposure_engines.dart';
 
 class UVController {
 
@@ -14,22 +14,25 @@ class UVController {
 
     final position = await locationService.getCurrentLocation();
 
-    
     final uvIndex = await apiService.fetchUVIndex(
       position.latitude,
       position.longitude,
     );
 
-    
     final riskLevel = UVRiskEngine.getRiskLevel(uvIndex);
 
+    // Calculate burn time based on UV index and skin type.
+    final burnTime = SunExposureEngine.calculateBurnTime(
+      uvIndex: uvIndex,
+      skinType: 3, // example skin type value
+    );
 
     final uvData = UVData(
       uvIndex: uvIndex,
       riskLevel: riskLevel,
+      burnTimeMinutes: burnTime,
       timestamp: DateTime.now(),
     );
-
     return uvData;
 
   }
